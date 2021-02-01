8b5e302194cc6878b4427ee34aeddacf56e2a39d
Hoang Anh Tuan <tuan-hoang@cybozu.vn>
2021-02-02 00:31:09 +0700

    ok

---
+++ src/components/Game.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Game = ()=>{
+    return (
+        <div
+        style={{
+            width: 300,
+            height: 500,
+            backgroundColor: 'pink',
+        }}>
+
+        </div>
+    )
+}
+
+export default Game;
